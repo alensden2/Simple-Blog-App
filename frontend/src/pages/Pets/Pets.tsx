7af8514a00f226a416ffc69fdd86b6a275fb9a62
alensden <alensden@gmail.com>
2023-03-12 17:22:48 -0300

    working on admin console UI

--- frontend/src/pages/Pets/Pets.tsx
+++ frontend/src/pages/Pets/Pets.tsx
@@ -1,57 +1,110 @@
-import React from 'react';
+import { Button, styled } from '@material-ui/core';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
+}));
+
+
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
+  createData('Tom', 'German Shepherd', 6.0, 'Male', '854613'),
+  createData('Jerry', 'Bulldog', 9.0, 'Male', '854613'),
+  createData('Zeus', 'Labrador Retriever', 16.0, 'Female', '854856'),
+  createData('Dooke', 'doberman', 3.7, 'Male', '658526'),
+  createData('Brownie', 'poodle', 16.0, 'Female', '789456')
 ];
 function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
-  ) {
-    return { name, calories, fat, carbs, protein };
+  name: string,
+  type: string,
+  age: number,
+  gender: string,
+  owner: string
+) {
+  return { name, type, age, gender, owner };
+}
+
+
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0
   }
+}));
 
 const AllPets: React.FC = () => {
   return (
     <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <h1 style={{ marginLeft: '20px' }}>Pet Console</h1>
+        <div style={{ marginRight: '20px' }}>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ marginRight: '10px' }}
+          >
+            Add Pet
+          </Button>
+          <Button variant="outlined" color="primary">
+            Delete all
+          </Button>
+        </div>
+      </div>
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <StyledTableCell align="center">Pet Name</StyledTableCell>
+              <StyledTableCell align="center">Type</StyledTableCell>
+              <StyledTableCell align="center">Age</StyledTableCell>
+              <StyledTableCell align="center">Gender</StyledTableCell>
+              <StyledTableCell align="center">Owner ID</StyledTableCell>
+              <StyledTableCell align="center">Edit</StyledTableCell>
+              <StyledTableCell align="center">Delete</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
                   {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.type}</StyledTableCell>
+                <StyledTableCell align="center">{row.age}</StyledTableCell>
+                <StyledTableCell align="center">{row.gender}</StyledTableCell>
+                <StyledTableCell align="center">{row.owner}</StyledTableCell>
+                <StyledTableCell align="center">
+                  <EditIcon />
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <DeleteIcon />
+                </StyledTableCell>
+              </StyledTableRow>
             ))}
           </TableBody>
         </Table>
