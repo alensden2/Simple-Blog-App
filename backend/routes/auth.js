ba3b661a6d1a2a22d829254760165c8a0073e492
Alen John <alensden@gmail.com>
2023-04-29 18:29:04 -0300

    hashing password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,16 +2,20 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Joi = require("@hapi/joi");
 const { registrationValidation } = require("./validation");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
   const { error } = registrationValidation(req.body);
 
   if (error) return res.status(400).send(error);
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   try {
