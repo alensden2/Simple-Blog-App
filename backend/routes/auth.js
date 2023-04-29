c3b94c33b4c0a1ece3a88c7cdf5b37aa94195767
Alen John <alensden@gmail.com>
2023-04-28 23:54:07 -0300

    authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,19 +1,30 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
 
 router.post("/register", async (req, res) => {
+  const { error } = await schema.validateAsync(req.body);
+
+  if (error) return res.status(400).send(error);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
 
-  try{
+  try {
     const saveUser = await user.save();
     console.log("user saved :", saveUser);
-    res.send(saveUser)
-  }catch(error){
-    res.status(400).send(error)
+    res.send(saveUser);
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
